fd5f04ec34fbe00f0e8a63620903b6e50a202901
Uffe Friis Lichtenberg <uffefl@gmail.com>
2023-03-26 11:20:34 +0200

    Trying to setup PWA

--- minesweeper.js
+++ minesweeper.js
@@ -6,6 +6,13 @@
 		return;
 	}
 
+	if ('serviceWorker' in navigator)
+	{
+		navigator.serviceWorker
+			.register('sw.js')
+			.then(() => { console.log('Service Worker Registered'); });
+	}
+
 	class MineSweeper
 	{
 		NumTiles;
